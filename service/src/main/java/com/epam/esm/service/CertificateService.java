003fc7d0781d7fded7c8b3c8ac908f14cf0a8b38
Andrey <Kuksa>
2021-08-13 20:02:39 +0300

    Working POST methods

--- service/src/main/java/com/epam/esm/service/CertificateService.java
+++ service/src/main/java/com/epam/esm/service/CertificateService.java
@@ -8,6 +8,7 @@ import com.epam.esm.mappers.CertificateEntityDtoMapper;
 import com.epam.esm.mappers.TagEntityDtoMapper;
 import com.epam.esm.repository.CertificateRepository;
 import com.epam.esm.repository.CertificateTagRepository;
+import com.epam.esm.repository.TagRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,11 +21,17 @@ import java.util.List;
 public class CertificateService {
 
     private final CertificateRepository certificateRepo;
+    private final TagEntityDtoMapper tagMapper;
+    private final TagRepository tagRepo;
     private final CertificateEntityDtoMapper certificateMapper;
     private final CertificateTagRepository certificateTagRepository;
-    private final TagEntityDtoMapper tagMapper;
 
     public void addCertificate(GiftCertificateDto certificateDto) {
+        List<TagDto> tags = certificateDto.getTags();
+        for(TagDto tagDto : tags){
+            GiftTag giftTag = tagMapper.tagDtoToTag(tagDto);
+            tagRepo.saveTag(giftTag);
+        }
         GiftCertificate giftCertificate = certificateMapper.certificateToCertificateDto(certificateDto);
         certificateRepo.addCertificate(giftCertificate);
     }
